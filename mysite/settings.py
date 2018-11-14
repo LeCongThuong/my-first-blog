331b4eb5bdcfc74f31e94eb95c5c4b128f232726
LeCongThuong <thuongle276@gmail.com>
2018-11-14 15:14:20 +0700

    Modified templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cy4($rb)d$-#_=b-h-z$kfndhrqwrk&#w)uqvejg5rrr(j1)+('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
